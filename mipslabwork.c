792457965da5c2a074bcc4240a2ae578725a79c2
root <root@DESKTOP-CJGSHEB>
2023-02-10 10:56:27 +0100

    change

--- mipslabwork.c
+++ mipslabwork.c
@@ -58,7 +58,7 @@ void labwork( void )
   delay( 1000 );
   time2string( textstring, mytime );
   display_string( 3, textstring );
-  display_update();
+  display_update(); 
   tick( &mytime );
   display_image(96, icon);
   *porte = ledTime;                                                    // Set let value to mytime.
